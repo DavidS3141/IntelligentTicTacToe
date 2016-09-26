cd713ddc76ab02b6de992b91988ee1b17365fcb7
Niklas Borchers <niklas.borchers@rwth-aachen.de>
2016-09-27 00:49:59 +0200

    Added GetGoodStates and GetBadStates

--- runner.cpp
+++ runner.cpp
@@ -1,27 +1,38 @@
 #include "runner.h"
 
-Runner::Runner(NeuralNetwork *n) :
-		network(n) {
-	runSimulation();
-}
+Runner::Runner(NeuralNetwork *n) : network(n) { runSimulation(); }
 
 void Runner::runSimulation() {
-	short player = 1;
-	short row, column;
-	short state = 0;
-	while (!state) {
-		network->getMove(game->getBoard(player), row, column);
-		moves.push_back(std::make_tuple(row, column, player));
-		state = game->makeMove(row, column, player);
-		player = (player % 2) + 1;
-	}
-	endState = state;
+  short player = 1;
+  short row, column;
+  short state = 0;
+  while (!state) {
+    network->getMove(game->getBoard(player), row, column);
+    moves.push_back(std::make_tuple(row, column, player));
+    state = game->makeMove(row, column, player);
+    player = (player % 2) + 1;
+  }
+  endState = state;
 }
 
 vector<State> Runner::getGoodStates() {
-	return vector<State>();
+  vector<State> states = std::vector<State>();
+  TicTacToe pseudoGame();
+  for (auto move : moves) {
+    if (player == endState | endState == -1 && player == circle)
+      states.push_back(std::make_pair(pseudoGame.getBoard(player), move));
+    pseudoGame.makeMove(move[0], move[1], move[2]);
+  }
+  return states;
 }
 
 vector<State> Runner::getBadStates() {
-	return vector<State>();
+  vector<State> states = std::vector<State>();
+  TicTacToe pseudoGame();
+  for (auto move : moves) {
+    if (player != endState | endState == -1 && player == cross)
+      states.push_back(std::make_pair(pseudoGame.getBoard(player), move));
+    pseudoGame.makeMove(move[0], move[1], move[2]);
+  }
+  return states;
 }
