e9658175dcd9ea2d95c76ccd61a14048222ca385
David Schmidt <david.schmidt@rwth-aachen.de>
2016-09-27 00:51:14 +0200

    ds: implement learning good moves

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+
 #include "NeuralNetwork.h"
+
+using namespace std;
 
 int main() {
 	NeuralNetwork* nn = new NeuralNetwork(27,100,9);
+	while(true){
+		int numSims;
+		cin >> numSims;
+		for(int i=0;i<numSims;++i){
+			Runner run(nn);
+			run.runSimulation();
+			vector<State> goodies=run.getGoodStates();
+			for(auto state : goodies){
+				nn->feedForward(getNodeBoard(state.first));
+				nn->backProp(getNodeMove(state.second), true); //TODO check with Marvin if correct parameter
+			}
+		}
+	}
 	return 0;
 }
