60f566076d902538833868310b210d078db48bb4
Niklas Borchers <niklas.borchers@rwth-aachen.de>
2016-09-27 00:17:13 +0200

    Base for implementation of runner

--- runner.cpp
+++ runner.cpp
@@ -1,3 +1,20 @@
 #include "runner.h"
 
-void Runner::runSimulation() {}
+void Runner::Runner(NeuralNetwork *n) : network(n) { runSimulation(); }
+
+void Runner::runSimulation() {
+  short player = 1;
+  short row, column;
+  short state = 0;
+  while (!state) {
+    network->getMove(game->getBoard(player), row, column);
+    moves.push_back(std::make_tuple(row, column, player));
+    state = game->makeMove(row, column, player);
+    player = (player % 2) + 1;
+  }
+  endState = state;
+}
+
+void Runner::getGoodStates() { return vector<States>(); }
+
+void Runner::getBadStates() { return vector<States>(); }
--- runner.h
+++ runner.h
@@ -1,14 +1,20 @@
 #ifndef RUNNER_H
 #define RUNNER_H
+#include "NeuralNetwork.h"
+#include "TicTacToe.h"
 #include <vector>
 
 class Runner {
-  Runner();
+  Runner(NeuralNetwork *);
+
+  vector<State> getBadStates();
+  vector<State> getGoodStates();
 
 private:
+  short endState;
+  TicTacToe game;
   vector<Moves> moves;
+  NeuronalNetwork *const network;
   void runSimulation();
-
-  vector<Moves> getMoves();
 };
 #endif
