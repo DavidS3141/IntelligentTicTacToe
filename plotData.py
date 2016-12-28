5df7937bf86a9f844fdf5298e7a13279202afc87
David Schmidt <david.schmidt@rwth-aachen.de>
2016-12-28 01:13:41 +0100

    quickfix number

--- plotData.py
+++ plotData.py
@@ -15,7 +15,7 @@ data = np.loadtxt('winSeries.txt',dtype=float)
 data[data==2.]=0.
 data[data==-1.]=0.5
 
-contractionNumber = min(2000,len(data)//2**4)
+contractionNumber = min(2500,len(data)//2**4)
 
 for i in range(4):
 	contracted = data[0:len(data)+1-contractionNumber]
@@ -23,8 +23,10 @@ for i in range(4):
 		contracted = contracted + data[j+1:len(data)+2+j-contractionNumber]
 	contracted = contracted/contractionNumber
 
-	plt.plot(np.arange(len(contracted))+contractionNumber/2, contracted, color=colors[i], label=contractionNumber)
+	plt.plot(np.arange(len(contracted))+contractionNumber/2, contracted*100., color=colors[i], label=contractionNumber)
 
 	contractionNumber *= 2
 
+plt.xlabel('game nbr')
+plt.ylabel('win rate [%]')
 plt.legend(loc='upper left')
